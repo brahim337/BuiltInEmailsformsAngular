d12487a171325d25a4875c8b8ee38be6957a02e2
unknown <echchakhaoui@gmail.com>
2020-01-25 00:04:21 +0100

    creation of the formGroup object in the AppcompTS and its html background

---
+++ BuiltInEmailForm/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "BuiltInEmailForm";
+  userEmails = new FormGroup({
+    primaryEmail: new FormControl(""),
+    secondaryEmail: new FormControl("")
+  });
+}
