2aa012ddba4c0f73832e6b8aacfd276a94296f62
unknown <echchakhaoui@gmail.com>
2020-01-25 02:02:56 +0100

    with reactive Built in element but it is not reactive

--- BuiltInEmailForm/src/app/app.component.ts
+++ BuiltInEmailForm/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -7,8 +7,21 @@ import { FormControl, FormGroup } from "@angular/forms";
 })
 export class AppComponent {
   title = "BuiltInEmailForm";
+  myRegExpression = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
+  get primEmail() {
+    return this.userEmails.get("primaryEmail");
+  }
+  get secondEmail() {
+    return this.userEmails.get("secondaryEmail");
+  }
   userEmails = new FormGroup({
-    primaryEmail: new FormControl(""),
-    secondaryEmail: new FormControl("")
+    primaryEmail: new FormControl("", [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")
+    ]),
+    secondaryEmail: new FormControl("", [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")
+    ])
   });
 }
--- BuiltInEmailForm/src/app/app.module.ts
+++ BuiltInEmailForm/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
