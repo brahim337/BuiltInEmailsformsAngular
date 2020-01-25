16d5e7ef27a93927cb0695fe474c447184aba41f
unknown <echchakhaoui@gmail.com>
2020-01-26 00:14:21 +0100

    Reactive built in email form works with email

--- BuiltInEmailForm/src/app/app.module.ts
+++ BuiltInEmailForm/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, NgForm } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent],
